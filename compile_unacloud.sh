3c52ec2af7208ade4bd89531d0584e79ed4bdd45
Jaime Chavarriaga <JaimeChavarriaga@users.noreply.github.com>
2017-10-09 09:07:48 -0500

    Changing package name in the resulting .deb

---
+++ compile_unacloud.sh
@@ -0,0 +1,11 @@
+# Unacloud compilation
+
+# generate the control file
+fakeroot debian/rules debian/control
+
+# remove the old files
+rm -rf debian/linux-image-extra-*/lib/modules/*/kernel/kernel
+
+# compile the kernel
+DEB_BUILD_OPTIONS=parallel=2 do_tools=false no_dumpfile=1 \
+   fakeroot debian/rules binary-generic
